20977824fdf68359e3e09a740f6a7824a1658d9f
yann <yann.r.berger@gmail.com>
2024-10-01 17:48:05 +0200

    some bouncing light

--- quad_shader.js
+++ quad_shader.js
@@ -21,6 +21,8 @@ struct Ray {
 struct RayTracingMaterial
 {
     vec4 color;
+    vec4 emissionColor;
+    float emissionStrength;
 };
 
 struct HitInfo {
@@ -40,6 +42,7 @@ struct Sphere
 
 uniform Sphere spheres[10];
 uniform int NumSpheres;
+uniform int MaxBounceCount;
 
 float RandomValue(inout uint state)
 {
@@ -47,6 +50,27 @@ float RandomValue(inout uint state)
     uint result = ((state >> ((state >> 28u) + 4u)) ^ state) * 277803737u;
     result = (result >> 22) ^ result;
     return float(result) / 4294967295.0;
+}
+
+float RandomValueNormalDistribution(inout uint state)
+{
+    float theta = 2.0 * 3.1415926 * RandomValue(state);
+    float rho = sqrt(-2.0 * log(RandomValue(state)));
+    return rho * cos(theta);
+}
+
+vec3 RandomDirection(inout uint state)
+{
+    float x = RandomValueNormalDistribution(state);
+    float y = RandomValueNormalDistribution(state);
+    float z = RandomValueNormalDistribution(state);
+    return normalize(vec3(x, y, z));
+}
+
+vec3 RandomHemisphereDirection(vec3 normal, inout uint rngState)
+{
+    vec3 dir = RandomDirection(rngState);
+    return dir * sign(dot(normal, dir));
 }
 
 HitInfo RaySphere(Ray ray, vec3 sphereCenter, float sphereRadius)
@@ -106,21 +130,44 @@ HitInfo CalculateRayCollision(Ray ray)
     return closestHit;
 }
 
+vec3 Trace(Ray ray, inout uint rngState)
+{
+    vec4 incomingLight = vec4(0.0, 0.0, 0.0, 0.0);
+    vec4 rayColor = vec4(1.0, 1.0, 1.0, 1.0);
+    for (int i = 0; i <= MaxBounceCount; i++)
+    {
+        HitInfo hitInfo = CalculateRayCollision(ray);
+        if (hitInfo.didHit)
+        {
+            ray.origin = hitInfo.hitPoint;
+            ray.dir = RandomHemisphereDirection(hitInfo.normal, rngState);
+
+            RayTracingMaterial material = hitInfo.material;
+            vec3 emittedLight = material.emissionColor.rgb * material.emissionStrength;
+            incomingLight += vec4(emittedLight, 0.0) * rayColor;
+            rayColor *= material.color;
+        } else { break; }
+    }
+    return incomingLight.rgb;
+}
+
 void main() {
     vec2 uv = (2.0 * gl_FragCoord.xy - resolution) / resolution.y;
+    // vec2 uv = gl_FragCoord.xy / resolution;
     vec3 viewPoint = vec3(uv, 1.0);
+
+    //test random value
+    vec2 numPixels = resolution.xy;
+    vec2 pixelCoords = gl_FragCoord.xy * numPixels;
+    float pixelIndex = pixelCoords.y * numPixels.x + pixelCoords.x;
+
+    uint rngState = uint(pixelIndex) * 2891336453u;
 
     Ray ray;
     ray.origin = vec3(0.0, 0.0, 0.0);
     ray.dir = normalize(viewPoint - ray.origin);
 
-    // HitInfo hitInfo = RaySphere(ray, vec3(0.0, 0.0, 5.0), 1.0);
-    HitInfo hitInfo = CalculateRayCollision(ray);
-
-    if (hitInfo.didHit) {
-        outColor = hitInfo.material.color;
-    } else {
-        outColor = vec4(0.0, 0.0, 0.0, 1.0);
-    }
+    vec3 pixelCol = Trace(ray, rngState);
+    outColor = vec4(pixelCol, 1);
 }
 `;
--- raytracing.js
+++ raytracing.js
@@ -9,25 +9,29 @@ let quad = new Rectangle(vertexShaderSource, fragmentShaderSource)
 
 //max 10, modify in shader if needed
 const spheresData = [
-    { pos: [0.5, 0.5, 1.0], radius: 0.2, material: { color: [1.0, 0.0, 0.0, 1.0] }},
-    { pos: [-0.5, -0.5, 1.5], radius: 0.2, material: { color: [0.0, 1.0, 0.0, 1.0] }},
-    { pos: [0.5, -0.5, 1.0], radius: 0.1, material: { color: [0.0, 0.0, 1.0, 1.0] }},
+    { pos: [0.9, 1.0, 2.0], radius: 1.0, material: { color: [1.0, 0.0, 0.0, 1.0], emissionColor : [1.0, 1.0, 1.0, 1.0], emissionStrength : 2.0}},
+    { pos: [-0.5, -0.3, 1.0], radius: 0.2, material: { color: [0.0, 1.0, 0.0, 1.0], emissionColor : [0.0, 0.0, 0.0, 0.0], emissionStrength : 0.0 }},
+    { pos: [0.5, -0.3, 1.0], radius: 0.1, material: { color: [0.0, 0.0, 1.0, 1.0], emissionColor : [0.0, 0.0, 0.0, 0.0], emissionStrength : 0.0}},
+    { pos: [0.0, -2.5, 1.5], radius: 2.0, material: { color: [0.0, 0.0, 1.0, 1.0], emissionColor : [0.0, 0.0, 0.0, 0.0], emissionStrength : 0.0}},
 ];
 
 function main() {
     resize()
     gl.clearColor(0.2, 0.2, 0.2, 1)
-
     update()
 }
 
 function update() {
+    let maxBounceCount = 10;
     gl.clear(gl.COLOR_BUFFER_BIT)
 
     gl.useProgram(quad.shader.program)
 
     let resolutionLocation = gl.getUniformLocation(quad.shader.program, "resolution")
     gl.uniform2f(resolutionLocation, canvas.width, canvas.height)
+
+    let maxBounceLocation = gl.getUniformLocation(quad.shader.program, "MaxBounceCount")
+    gl.uniform1i(maxBounceLocation, maxBounceCount)
 
     setSpheres(quad.shader.program, spheresData)
     
@@ -57,6 +61,12 @@ function setSpheres(program, spheres)
 
         let colorLocation = gl.getUniformLocation(program, `spheres[${i}].material.color`);
         gl.uniform4fv(colorLocation, sphere.material.color);
+
+        let colorEmissionLocation = gl.getUniformLocation(program, `spheres[${i}].material.emissionColor`);
+        gl.uniform4fv(colorEmissionLocation, sphere.material.emissionColor);
+
+        let emissionStrengthLocation = gl.getUniformLocation(program, `spheres[${i}].material.emissionStrength`);
+        gl.uniform1f(emissionStrengthLocation, sphere.material.emissionStrength);
     }
 
     let numSpheresLocation = gl.getUniformLocation(program, 'NumSpheres');
